29c74bb5b079540e672be1090528b07c10e15595
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2023-11-03 09:31:43 +0700

    i

---
+++ BusinesssLayer/View_FUNC_SYS_RIGHT.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinesssLayer
+{
+    public class View_FUNC_SYS_RIGHT
+    {
+        Entities db;
+        public View_FUNC_SYS_RIGHT()
+        {
+            db = Entities.CreateEntities();
+        }
+        public List<V_FUNC_SYS_RIGHT>getFuncByUser(int idUser)
+        {
+            return db.V_FUNC_SYS_RIGHT.Where(x=>x.IDUSER == idUser).OrderBy(x=>x.SORT).ToList();
+        }
+    }
+}

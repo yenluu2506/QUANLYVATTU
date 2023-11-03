9caca568e8b45ec2440aa36b22dedccb2cd93bc3
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2023-11-04 01:03:58 +0700

    Update Phân Quyền

--- BusinesssLayer/SYS_RIGHT.cs
+++ BusinesssLayer/SYS_RIGHT.cs
@@ -13,6 +13,10 @@ namespace BusinesssLayer
         public SYS_RIGHT() {
             db = Entities.CreateEntities(); 
         }
+        public tb_SYS_RIGHT getRight(int idUser, string func_code)
+        {
+            return db.tb_SYS_RIGHT.FirstOrDefault(x=>x.IDUSER==idUser && x.FUNC_CODE==func_code);
+        }
         public void update(int idUser, string func_code, int right)
         {
             tb_SYS_RIGHT sRight = db.tb_SYS_RIGHT.FirstOrDefault(x=>x.IDUSER == idUser && x.FUNC_CODE == func_code);

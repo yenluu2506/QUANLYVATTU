9b87385e811974682acc9f3125941f528bc5043f
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2023-11-03 23:34:01 +0700

    update phân quyền báo cáo

---
+++ USERMANAGEMENT/frmPhanQuyenBaoCao.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USERMANAGEMENT
+{
+    public partial class frmPhanQuyenBaoCao : DevExpress.XtraEditors.XtraForm
+    {
+        public frmPhanQuyenBaoCao()
+        {
+            InitializeComponent();
+        }
+    }
+}

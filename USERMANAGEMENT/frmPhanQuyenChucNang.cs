762c7b4cca9149dd8ebd2e66bb7eb3217cb3daad
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2023-11-03 09:31:43 +0700

    i

---
+++ USERMANAGEMENT/frmPhanQuyenChucNang.cs
@@ -0,0 +1,71 @@
+﻿using BusinesssLayer;
+using DataLayer;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USERMANAGEMENT
+{
+    public partial class frmPhanQuyenChucNang : DevExpress.XtraEditors.XtraForm
+    {
+        public frmPhanQuyenChucNang()
+        {
+            InitializeComponent();
+        }
+        public int _idUser;
+        public string _macty;
+        public string _madvi;
+        SYS_USER _sysUser;
+        tb_SYS_RIGHT _sysRight;
+
+        private void frmPhanQuyenChucNang_Load(object sender, EventArgs e)
+        {
+            _sysRight = new tb_SYS_RIGHT();
+            _sysUser = new SYS_USER();
+            loadUsers();
+            loadFuncByUser();
+        }
+        void loadUsers()
+        {
+            if (_macty == null && _madvi == null)
+            {
+                gcUsers.DataSource = _sysUser.getUserByDViFunc("CT01", "~");
+                gvUsers.OptionsBehavior.Editable = false;
+            }
+            else
+            {
+                gcUsers.DataSource = _sysUser.getUserByDViFunc(_macty, _madvi);
+                gvUsers.OptionsBehavior.Editable = false;
+            }
+        }
+        void loadFuncByUser()
+        {
+            View_FUNC_SYS_RIGHT _vFuncRight = new View_FUNC_SYS_RIGHT();
+            gcChucNang.DataSource = _vFuncRight.getFuncByUser(_idUser);
+            gvChucNang.OptionsBehavior.Editable = false;
+        }
+
+        private void gvUsers_CustomDrawCell(object sender, DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs e)
+        {
+            if (e.Column.Name == "ISGROUP" && bool.Parse(e.CellValue.ToString()) == true)
+            {
+                Image img = Properties.Resources.Team_16x16;
+                e.Graphics.DrawImage(img, e.Bounds.X, e.Bounds.Y);
+                e.Handled = true;
+            }
+            if (e.Column.Name == "ISGROUP" && bool.Parse(e.CellValue.ToString()) == false)
+            {
+                Image img = Properties.Resources.User_16x16;
+                e.Graphics.DrawImage(img, e.Bounds.X, e.Bounds.Y);
+                e.Handled = true;
+            }
+        }
+    }
+}
